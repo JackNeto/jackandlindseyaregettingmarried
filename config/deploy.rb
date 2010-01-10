7e814ee291b88b1888812d0c2d11868141d2a602
Jack Neto <jack@theworkinggroup.ca>
2010-01-10 10:26:50 -0500

    Added deployment config

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,53 +1,41 @@
 default_run_options[:pty] = true
 
+set :application, 'jackandlindseyaregettingmarried'
 
-set :application, "wgtg-subscription"
-
-set :scm, :git
-set :repository,  "git@twg.unfuddle.com:twg/#{application}.git"
+set :scm,         :git
+set :repository,  'git@github.com:JackNeto/jackandlindseyaregettingmarried.git'
 set :deploy_via,  :remote_cache
 
-set :user, "deploy"
-set :use_sudo, false
-set :keep_releases, 2
+set :user,      'deploy'
+set :use_sudo,  false
 
-set :app_symlinks,  %w{ }
-
-
-
-task :staging do
-  set :rails_env, 'staging'
-  set :domain, "staging.theworkinggroup.ca"
-  set :deploy_to, "/web/#{application}.theworkinggroup.ca"
-  set :branch, "master"
-  set :keep_releases, 1
-  
-  role :app, domain
-  role :web, domain  
-  role :db,   domain, :primary => true
-end
-
-
+set :keep_releases, 3
 
 task :production do
-  set :rails_env, 'production'
-  set :domain, "rails.theworkinggroup.ca"
-  set :deploy_to, "/web/charities.wegivetoget.com"
-  set :branch, "master"
-  role :app, domain
-  role :web, domain  
+  set :rails_env,   'production'
+  set :domain,      "#{application}.ca"
+  set :deploy_to,   "/web/#{application}"
+  set :branch,      'deploy'
+  
+  role :app,  domain
+  role :web,  domain
   role :db,   domain, :primary => true
-  after "deploy:symlink", "deploy:update_crontab"
 end
 
+
 namespace :deploy do
-  task :restart, :roles => :app do
+  
+  task :start do ; end
+  task :stop  do ; end
+  
+  task :restart, :roles => :app, :except => { :no_release => true } do
     run "touch #{release_path}/tmp/restart.txt"
   end
   
-  desc "Update the crontab file"
-  task :update_crontab, :roles => :db do
-    run "cd #{release_path} && whenever --update-crontab #{application}"
+  task :apply_configs, :roles => :app do
+    run "cp #{release_path}/config/environments/#{rails_env}/database.yml #{release_path}/config/database.yml"
   end
-  
 end
+
+after 'deploy:update_code', 'deploy:apply_configs'
+after 'deploy', 'deploy:cleanup'

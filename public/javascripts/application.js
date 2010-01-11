3a2141e18b9553f0154af06e6611727c1f9de7bf
Jack Neto <jack@theworkinggroup.ca>
2010-01-10 23:08:13 -0500

    Added counter

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,5 @@
-// Place your application-specific JavaScript functions and classes here
-// This file is automatically included by javascript_include_tag :defaults
+$().ready(function() {
+  var newYear = new Date(2010, 6, 17, 14, 0, 0); 
+  newYear = new Date(newYear); 
+  $('#defaultCountdown').countdown({until: newYear, layout: '{dn} {dl}, {hn} {hl}, {mn} {ml} and {sn} {sl}'});
+});

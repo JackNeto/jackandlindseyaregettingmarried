2b0b7175a90a0be3d900123c590da0dd3719c0d0
Jack Neto <jack@theworkinggroup.ca>
2010-01-26 01:10:28 -0500

    Small fix

--- app/controllers/cms_admin/base_controller.rb
+++ app/controllers/cms_admin/base_controller.rb
@@ -4,6 +4,15 @@ class CmsAdmin::BaseController < ApplicationController
   before_filter :is_admin?
 
   layout 'cms_admin'
+  
+  def manage_session_array(name, action, value)
+    session[name] = case action
+    when :add
+      (session[name] || []) + [value]
+    when :remove
+      (session[name] || []) - [value]
+    end
+  end
 
 protected
 

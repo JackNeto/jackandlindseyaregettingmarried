192ef1f060424c77a2986ef3a6471ec0c8a6aac0
Jack Neto <jack@theworkinggroup.ca>
2010-01-28 17:13:28 -0500

    Updated hotoad

--- config/deploy.rb
+++ config/deploy.rb
@@ -48,3 +48,10 @@ end
 after 'deploy:update_code', 'deploy:apply_configs'
 after "deploy:update_code", "deploy:asset:symlink"
 after 'deploy', 'deploy:cleanup'
+
+
+Dir[File.join(File.dirname(__FILE__), '..', 'vendor', 'gems', 'hoptoad_notifier-*')].each do |vendored_notifier|
+  $: << File.join(vendored_notifier, 'lib')
+end
+
+require 'hoptoad_notifier/capistrano'
--- config/environment.rb
+++ config/environment.rb
@@ -12,6 +12,8 @@ Rails::Initializer.run do |config|
   config.gem 'mislav-will_paginate',  :lib => 'will_paginate'
   config.gem 'active_link_to'
   config.gem 'comfortable_mexican_sofa'
+  config.gem 'hoptoad_notifier'
+  
   
   config.time_zone = 'UTC'
 end
